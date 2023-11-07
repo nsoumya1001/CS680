a202a3df6963417ad00b3bab9e08fb1a254feac0
nsoumya1001 <114720845+nsoumya1001@users.noreply.github.com>
2023-11-07 15:08:46 -0500

    Delete HW05  directory

--- HW05 /src/edu/umb/cs680/hw05/StoryObserver.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw05;
-
-public class StoryObserver implements Observer<InstagramUserEvent>{
-
-    @Override
-    public void update (Observable<InstagramUserEvent> sender, InstagramUserEvent event) {
-        System.out.println ("The Story update:"+ event.getUpdate ());
-    }
-}
